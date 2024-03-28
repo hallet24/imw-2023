f4a896cb4ebf298bde208b447763bc422c9e9a5b
halleturner <eliz24.turner@gmail.com>
2024-03-28 17:36:17 -0400

    Completed E8: jSON and added it to the main webpage/index

--- Exercises/E8/script.js
+++ Exercises/E8/script.js
@@ -18,20 +18,77 @@ console.log('test');
 
 // const inputField = document.getElementById("button")
 
-fetch(`https://api.lyrics.ovh/v1/[{artist}/{title}]${searchTerm}`);
-    .then(function (application) {
-    return application.json();
-})
-    .then(function (data) {
-        console.log(data);
-        data.forEach(dataPoint => {
-            $(`body`).append(`
-        <div>
-        <h2>${dataPoint.chorus}</h2>
-        </div>`)
-            console.log(dataPoint);
+
+
+// Call the random_bg_color function to set a random background color.
+// random_bg_color();
+
+function getRandomId() {
+    return Math.floor(Math.random() * 826);
+}
+
+function getCharacter(id) {
+    return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(function (response) {
+            return response.json();
         })
-    })
-    .catch(function (error) {
-        // Display error message
-    })
+}
+// function random_bg_color() {
+//     var x = Math.floor(Math.random() * 256);
+//     var y = Math.floor(Math.random() * 256);
+//     var z = Math.floor(Math.random() * 256);
+//     // Construct the RGB color string.
+//     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+//     // Output the generated color to the console.
+//     console.log(bgColor);
+//     // Set the background color of the document body to the generated color.
+//     document.body.style.background = bgColor;
+// }
+
+
+$(document).ready(function () {
+    console.log('HTML finished loading...');
+
+    const container = $('#my-container');
+
+    $('#my-button').on('click', function () {
+        getCharacter(getRandomId()).then(function (data) {
+            console.log('got something');
+            console.log(data);
+            container.html(`<div class="content">
+                <div><strong>Name: </strong>${data.name}</div>
+                <div><strong>ID: </strong>${data.id}</div>
+                <div><strong>Status: </strong>${data.status}</div>
+                <div><img src="${data.image}"/></div>
+            </div>`)
+        })
+        $('#my-button').on("click", function () {
+            const curColour = document.body.style.backgroundColor;
+
+            var x = Math.floor(Math.random() * 256);
+            var y = Math.floor(Math.random() * 256);
+            var z = Math.floor(Math.random() * 256);
+
+            var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+            document.body.style.backgroundColor = bgColor;
+        });
+    });
+
+});
+
+// var button = document.querySelector("my-button");
+
+// button.addEventListener("click", function () {
+//     const curColour = document.body.style.backgroundColor;
+
+//     // var x = Math.floor(Math.random() * 256);
+//     // var y = Math.floor(Math.random() * 256);
+//     // var z = Math.floor(Math.random() * 256);
+
+//     // var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+//     document.body.style.backgroundColor = bgColor;
+// })
+
+const searchTerm = 'hello';
